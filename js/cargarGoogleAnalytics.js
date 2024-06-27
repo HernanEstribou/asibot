a5e98df09ef9b249ee38b61f4724064d4fe95dc5
HernanEstribou <hernanestribou@gmail.com>
2024-06-27 19:24:52 -0300

    Externalización de ID de Google Analitycs en archivo JSON

---
+++ js/cargarGoogleAnalytics.js
@@ -0,0 +1,28 @@
+async function loadGoogleAnalytics() {
+    try {
+      const response = await fetch('./config.json');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const config = await response.json();
+      const googleAnalyticsId = config.googleAnalyticsId;
+      console.log(googleAnalyticsId);
+
+      // Cargar el script de Google Analytics con el ID recuperado
+      const gtagScript = document.createElement('script');
+      gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`;
+      gtagScript.async = true;
+      document.head.appendChild(gtagScript);
+  
+      // Configurar Google Analytics
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', googleAnalyticsId);
+    } catch (error) {
+      console.error('Error loading Google Analytics config:', error);
+    }
+  }
+  
+  // Llamar a la función para cargar y configurar Google Analytics
+  loadGoogleAnalytics();
